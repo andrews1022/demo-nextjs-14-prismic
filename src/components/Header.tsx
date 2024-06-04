a5f9eb208ff3f3512ef077236c591eb025d814f5
Andrew Shearer <andrewshearerdev@gmail.com>
2024-06-04 15:36:54 -0700

    updated header component to use navigation from settings document

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,29 @@
-const Header = () => {
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+
+import { createClient } from "@/prismicio";
+
+const Header = async () => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <div>
-      <p>Header</p>
+      <Link href="/">Home</Link>
+
+      <nav>
+        <ul>
+          {settings.data.navigation.map((item) => {
+            const { label, link } = item;
+
+            return (
+              <li key={label}>
+                <PrismicNextLink field={link}>{label}</PrismicNextLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
   );
 };
